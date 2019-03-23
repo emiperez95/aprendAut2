8d13f181b3722981fb58d131353303a57843601a
fchavat <felipe.chavat@gmail.com>
2019-03-23 20:49:41 -0300

    Without errors

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from tree import tree
 evaluationData = "data/evaluationData.npy"
 trainingData = "data/trainingData.npy"
 
+fakeSet = np.array([
+  ['1', '1'],
+  ['2', '1'],
+  ['3', '1'],
+  ['0', '2'],
+
+])
 data = np.load(trainingData)
 
 # Train models
-treeObject = tree(data)
+treeObject = tree(data.astype(float))
 
 # Evaluate models
--- tree.py
+++ tree.py
@@ -12,14 +12,14 @@ class Tree(object):
 
 class tree:
   def __init__ (self,trainingData):
-    self.entropy = self.__shannonEntropy(trainingData)
-    self.lengthTS = len(trainingData)
+    self.entropy = float(self.__shannonEntropy(trainingData))
+    self.lengthTS = float(len(trainingData))
 
     self.train(trainingData)
 
   def __shannonEntropy(self, examples):
     entropy = 0
-    length = float(len(examples))
+    length = float(len(examples)) # Si abajo en Pi = .. uno de los dos no es float, da 0 como resultado por ser ints
     counts = self.__countOfEachClass(examples)
     for count in counts:
       Pi = count[1]/length
@@ -35,7 +35,7 @@ class tree:
       #         minInfoGain = infoGain
       #         minInfoGainVal = val #TODO:
 
-      print(self.gainAndThreshold(data, 0))
+    # print(self.__gainAndThreshold(data, 0))
       # self.__id3(data, 4, [0,1,2,3])
 
   def __id3(self, examples, target_attribute, attributes):
@@ -47,25 +47,34 @@ class tree:
     if (len(attributes) == 0):
       newRootNode.label = self.__mostCommonValueFor(examples, target_attribute) #TODO:
       return newRootNode
-    bestAttribute, thresholds = self.__bestFitAttribute(examples, attributes)
+    bestAttribute, threshold = self.__bestFitAttribute(examples, attributes)
 
 
   def __bestFitAttribute(self, examples, attributes):
-    # Si entra aca es porque hay al menos 2 ejemplos con distinta clase
+    # Si entra aca es porque hay attributes y al menos 2 ejemplos con distinta clase
     # Sino hubiese entrado en un caso base.
-    pass
+    bestIG = None
+    bestThreshold = None
+    bestAttribute = None
+    for attr in attributes:
+      IG, threshold = self.__gainAndThreshold(examples, attr)
+      if bestIG == None or bestIG < IG:
+        bestIG = IG
+        bestThreshold = threshold
+        bestAttribute = attr
+    return bestAttribute, threshold
 
-  def gainAndThreshold(self, examples, attribute):
+  def __gainAndThreshold(self, examples, attribute):
     # Lets get the possible thresholds
-    sortedExamples = np.sort(examples, axis=attribute)
+    sortedExamples = examples[np.argsort(examples[:,attribute])]
     possibleThresholds = []
-    lastClass = float(sortedExamples[0][-1])
-    lastAttrValue = float(sortedExamples[0][attribute])
+    lastClass = sortedExamples[0][-1]
+    lastAttrValue = sortedExamples[0][attribute]
     for row in sortedExamples[1:]:
       if row[-1] != lastClass:
-        possibleThresholds.append((float(row[attribute]) - lastAttrValue)/2)
-      lastClass = float(row[-1])
-      lastAttrValue = float(row[attribute])
+        possibleThresholds.append((row[attribute] + lastAttrValue)/2)
+      lastClass = row[-1]
+      lastAttrValue = row[attribute]
 
     # Lets get the best threshold
     bestThreshold = None
@@ -78,9 +87,9 @@ class tree:
 
       # IG(T, a) = H(T) - H(T|a)
       # H(T|a) = para todo v posible de vals(a) SUM((|Sa(v)|/|T|)*H(Sa(v)))
-      IG = TEntropy
-      - (len(partitionLess)/self.lengthTS)*self.__shannonEntropy(partitionLess)
-      - (len(partitionEqualGreat)/self.lengthTS)*self.__shannonEntropy(partitionEqualGreat)
+      HLess = (len(partitionLess)/self.lengthTS)*self.__shannonEntropy(partitionLess)
+      HEqualGreat = (len(partitionEqualGreat)/self.lengthTS)*self.__shannonEntropy(partitionEqualGreat)
+      IG = TEntropy - HLess - HEqualGreat
 
       if (bestIG == None or bestIG < IG):
         bestThreshold = threshold
@@ -88,78 +97,12 @@ class tree:
 
     return bestIG, bestThreshold
 
-
   def __partition(self, examples, attribute, threshold):
-    attrCol = examples[:,attribute].astype(np.float)
-    condArr = attrCol < threshold
-    return attrCol[condArr], attrCol[~condArr]
+    condArr = examples[:,attribute] < threshold
+    return examples[condArr], examples[~condArr]
 
   def __countOfEachClass(self, examples):
     values = examples[:,-1].astype(np.int)
     countPerValue = np.bincount(values)
     countFiltered = np.nonzero(countPerValue)[0]
     return list(zip(countFiltered, countPerValue[countFiltered]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  def clasificarCol(self,col, clase):
-      # Establezco como clasificar
-      minimo = np.min(col)
-      maximo = np.max(col)
-      umbral = (maximo - minimo)/2
-      return self.infoGain(col, clase, umbral)
-
-  def infoGain(self, col, clase, umbral):
-      bajo, alto, dictBajo, dictAlto = self.cantidadUmbral(col,clase,umbral)
-      total = len(col)
-
-      propBajo = bajo/total
-      propAlto = alto/total
-
-      entropiaTotalBajo = 0
-      entropiaTotalAlto = 0
-
-      for val in dictBajo.values():
-          prop = val/bajo
-          entropiaTotalBajo -=  prop * np.log(prop)
-
-      for val in dictAlto.values():
-          prop = val/alto
-          entropiaTotalAlto -=  prop * np.log(prop)
-
-      entropiaTotal = propBajo * entropiaTotalBajo + propAlto * entropiaTotalAlto
-
-      return entropiaTotal
-
-  def cantidadUmbral(self, col,clase, umbral):
-      bajo = 0
-      alto = 0
-      dictBajo = {}
-      dictAlto = {}
-      for ind,cell in enumerate(col):
-          if cell > umbral:
-              if clase[ind] in dictBajo:
-                  dictBajo[clase[ind]] += 1
-              else:
-                  dictBajo[clase[ind]] = 1
-              bajo += 1
-
-          else:
-              if clase[ind] in dictAlto:
-                  dictAlto[clase[ind]] += 1
-              else:
-                  dictAlto[clase[ind]] = 1
-              alto += 1
-      return bajo, alto, dictBajo, dictAlto
